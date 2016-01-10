2cc35678621c537528b0523d00ceb5486d6819ff
Jose douglas ramirez <chibchombiano26@hotmail.com>
2016-01-10 02:09:14 +0000

    Webpack

---
+++ js/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from './components/Hello';
+
+
+/*
+let Hello = React.createClass({
+    render(){
+        return <h3>Hello ES6</h3>;
+    }
+});
+*/
+
+
+
+
+ReactDOM.render(<Hello />, document.getElementById('react'));
---
+++ js/components/Hello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Hello extends React.Component{
+    render(){
+        return <h3> Hello React with ES6 </h3>
+    }
+}
+
+export default Hello;
